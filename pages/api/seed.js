41fa8a155dc68134e7bb2f08e622f7ad5b41cc71
paixaoariellll <paixaoariellll@gmail.com>
2022-12-29 14:59:26 -0300

    feedback 5.0

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,6 +1,6 @@
-import Feedback from "../../models/feedback";
-import data from "../../utils/data";
-import db from "../../utils/db";
+import Feedback from '../../models/feedback';
+import data from '../../utils/data';
+import db from '../../utils/db';
 
 const handler = async (req, res) => {
   await db.connect();
