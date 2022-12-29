df58a45844ddb16016d5fb2f73ca7c8eebca39b3
paixaoariellll <paixaoariellll@gmail.com>
2022-12-29 14:47:24 -0300

    feedback

--- pages/api/feedback/[id]/index.js
+++ pages/api/feedback/[id]/index.js
@@ -2,29 +2,28 @@ import Feedback from '../../../../models/feedback';
 import db from '../../../../utils/db';
 
 const handler = async (req, res) => {
-    await db.connect();
-    const feedback = new Feedback({
-        nome: req.body.nome,
-        avaliação: req.body.avaliação,
-        comentário: req.body.comentário,
-    });
-    const savedFeedback = await feedback.save();
-    await db.disconnect();
-
-    res.send(savedFeedback);
+  await db.connect();
+  const feedback = new Feedback({
+    nome: req.body.nome,
+    avaliação: req.body.avaliação,
+    comentário: req.body.comentário,
+  });
+  const savedFeedback = await feedback.save();
+  await db.disconnect();
+  res.send(savedFeedback);
 };
 
 const deleteHandler = async (req, res) => {
-    await db.connect();
-    const feedback = await Feedback.findById(req.query.id);
-    if (feedback) {
-        await feedback.remove();
-        await db.disconnect();
-        res.send({ message: 'Feedback deletado com sucesso!' });
-    } else {
-        await db.disconnect();
-        res.status(404).send({ message: 'Feedback não encontrado!' });
-    }
+  await db.connect();
+  const feedback = await Feedback.findById(req.query.id);
+  if (feedback) {
+    await feedback.remove();
+    await db.disconnect();
+    res.send({ message: 'Feedback deletado com sucesso!' });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Feedback não encontrado!' });
+  }
 };
 
 export default handler;
--- pages/api/feedback/index.js
+++ pages/api/feedback/index.js
@@ -2,33 +2,33 @@ import Feedback from '../../../models/feedback';
 import db from '../../../utils/db';
 
 const handler = async (req, res) => {
-    if (req.method === "GET") {
-        return getHandler(req, res);
-    } if (req.method === "POST") {
-        return postHandler(req, res);
-    }
-    return res.status(400).send({ message: 'Deu merda!' });
+  if (req.method === "GET") {
+    return getHandler(req, res);
+  } if (req.method === "POST") {
+    return postHandler(req, res);
+  }
+  return res.status(400).send({ message: 'Deu merda!' });
 
 };
 
 // Criando um novo comentário
 const postHandler = async (req, res) => {
-    await db.connect();
-    const newFeedback = new Feedback({
-        nome: 'Anônimo',
-        avaliacao: 5,
-        comentário: 'Escreva seu comentário',
-    });
-    const feedbacks = await newFeedback.save();
-    await db.disconnect();
-    res.send({ message: 'Comentário adicionado com sucesso!', feedbacks });
+  await db.connect();
+  const newFeedback = new Feedback({
+    nome: 'Anônimo',
+    avaliacao: 5,
+    comentário: 'Escreva seu comentário',
+  });
+  const feedbacks = await newFeedback.save();
+  await db.disconnect();
+  res.send({ message: 'Comentário adicionado com sucesso!', feedbacks });
 };
 
 const getHandler = async (req, res) => {
-    await db.connect();
-    const feedbacks = await Feedback.find({});
-    await db.disconnect();
-    res.send(feedbacks);
+  await db.connect();
+  const feedbacks = await Feedback.find({});
+  await db.disconnect();
+  res.send(feedbacks);
 };
 
 export default handler;
